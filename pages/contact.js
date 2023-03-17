67e0e255d808717634b888c0e3bedbfe2c1dd35e
DESKTOP-DC91P5K\user <satou1497@gmail.com>
2023-03-17 09:50:05 +0900

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,35 @@
 export default function Contact() {
   return (
     <>
-    Contact
+      <form name="contact" method="POST" netlify>
+        <p>
+          <label className="block float-left w-20">
+            名前：
+          </label>
+            <input type="text" name="name" required
+              className="box-border border-2 border-gray-600"
+            />
+        </p>
+        <p className="mt-2">
+          <label className="block float-left w-20">
+            Eメール：
+          </label>
+            <input type="email" name="email"  required
+              className="box-border border-2 border-gray-600"
+              />
+        </p>
+        <p className="mt-2">
+          <label className="block float-left w-20">
+            内容：
+          </label>
+            <textarea  name="name" cols="50" rows="10"  required
+              className="box-border border-2 border-gray-600"
+              />
+        </p>
+        <p className="mt-2">
+          <button type="submit" className="tw-btn-primary">Send</button>
+        </p>
+      </form>
     </>
   )
 }
--- pages/jigyou.js
+++ pages/jigyou.js
@@ -1,7 +1,85 @@
+import Image from 'next/image'
+
 export default function Jigyou() {
   return (
     <>
-    Jigyou
+      <h1>定款</h1>
+      <p>「定款」PDFファイル　
+        <a href='/pdf/kyouyou.pdf' target="_blank">
+            <Image src="/images/pdf_symbol.gif"
+              alt="menu"
+              width={32} height={10}
+              className="ml-10 mt-3 inline"
+            ></Image>
+          </a>
+      </p>
+      <h1>役員名簿</h1>
+      <p>「役員名簿」PDFファイル　
+        <a href='/pdf/kyouyou.pdf' target="_blank">
+            <Image src="/images/pdf_symbol.gif"
+              alt="menu"
+              width={32} height={10}
+              className="ml-10 mt-3 inline"
+            ></Image>
+          </a>
+      </p>
+      <h1>役員の報酬基準</h1>
+      <p>「役員の報酬基準」PDFファイル　
+        <a href='/pdf/kyouyou.pdf' target="_blank">
+            <Image src="/images/pdf_symbol.gif"
+              alt="menu"
+              width={32} height={10}
+              className="ml-10 mt-3 inline"
+            ></Image>
+          </a>
+      </p>
+      <h1>会員名簿</h1>
+      <p>「会員名簿」PDFファイル　
+        <a href='/pdf/kyouyou.pdf' target="_blank">
+            <Image src="/images/pdf_symbol.gif"
+              alt="menu"
+              width={32} height={10}
+              className="ml-10 mt-3 inline"
+            ></Image>
+          </a>
+      </p>
+      <h1>事業報告等</h1>
+      <table width="100%" className="ml-4">
+        <tbody>
+          <tr>
+            <td>　種　　類</td>
+            <td width="13%">平成<br />29年度</td>
+            <td width="13%">平成<br />30年度</td>
+            <td width="13%">令和<br />元年度</td>
+            <td width="13%">令和<br />2年度</td>
+            <td width="13%">令和<br />3年度</td>
+          </tr>
+          <tr>
+            <td>事業報告書</td>
+            <td> <a href="http://88gunma.or.jp/document/h30/jigyou.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r01/jigyou.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r02/jigyou.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r03/jigyou.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r04/jigyou.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+          </tr>
+          <tr>
+            <td>正味財産増減<br />計算書</td>
+            <td> <a href="http://88gunma.or.jp/document/h30/zaisan2.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r01/zaisan.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r02/zaisan.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r03/zaisan.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+            <td> <a href="http://88gunma.or.jp/document/r04/zaisan.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+          </tr>
+          <tr>
+            <td>貸借対照表</td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> <a href="http://88gunma.or.jp/document/r04/taisyaku.pdf" target="_blank" rel="noopener noreferrer"><img class="alignnone size-full wp-image-37" src="/images/pdf_symbol.gif" alt="pdf_symbol" width="34" height="32" /></a></td>
+          </tr>
+        </tbody>
+      </table>
     </>
   )
 }
